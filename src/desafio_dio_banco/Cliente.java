2e08f961883245fb207d2f958d56f3a7f374ca60
Andredd2 <andre.576@hotmail.com>
2024-08-08 23:47:39 -0300

    Expert talk 08/08/2024

---
+++ src/desafio_dio_banco/Cliente.java
@@ -0,0 +1,19 @@
+package desafio_dio_banco;
+
+public class Cliente {
+	
+	private String nome;
+	private String cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
+
